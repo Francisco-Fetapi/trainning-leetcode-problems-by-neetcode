66366e67be7559f7c2bb4f56627132c77526f15c
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-06-19 18:54:41 +0100

    feat: product of array exclude itself solved

--- src/arrays_and_hash/__test__/productOfArray.spec.ts
+++ src/arrays_and_hash/__test__/productOfArray.spec.ts
@@ -1,4 +1,4 @@
-import { productOfArray1 } from "../productOfArray";
+import { productOfArray1, productOfArray2 } from "../productOfArray";
 
 const case1 = [1, 2, 3, 4];
 
@@ -6,7 +6,7 @@ describe("isAnagram", () => {
   it("First Solution", () => {
     expect(productOfArray1(case1)).toEqual([24, 12, 8, 6]);
   });
-  // it("Second Solution", () => {
-
-  // });
+  it("Second Solution", () => {
+    expect(productOfArray2(case1)).toEqual([24, 12, 8, 6]);
+  });
 });
